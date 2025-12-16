36f255780b3b35cf2b18ec8bdd07327fdf8b1033
Aman Kumar Singh <singhaman13720@gmail.com>
2025-12-16 16:07:09 +0530

    solving missing coin sum

---
+++ MissingCoinSum/MissingCoinSum.java
@@ -0,0 +1,89 @@
+package MissingCoinSum;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class MissingCoinSum {
+    static class FastScanner {
+        private final InputStream in = System.in;
+        private final byte[] buffer = new byte[1 << 16];
+        private int ptr = 0, len = 0;
+
+        private int read() throws IOException {
+            if (ptr >= len) {
+                len = in.read(buffer);
+                ptr = 0;
+                if (len <= 0)
+                    return -1;
+            }
+            return buffer[ptr++];
+        }
+
+        int nextInt() throws IOException {
+            int c;
+            while ((c = read()) <= ' ')
+                if (c == -1)
+                    return -1;
+            int sign = 1;
+            if (c == '-') {
+                sign = -1;
+                c = read();
+            }
+            int val = c - '0';
+            while ((c = read()) > ' ')
+                val = val * 10 + (c - '0');
+            return val * sign;
+        }
+
+        long nextLong() throws IOException {
+            int c;
+            while ((c = read()) <= ' ')
+                if (c == -1)
+                    return -1;
+            int sign = 1;
+            if (c == '-') {
+                sign = -1;
+                c = read();
+            }
+            long val = c - '0';
+            while ((c = read()) > ' ')
+                val = val * 10 + (c - '0');
+            return val * sign;
+        }
+        String nextString() throws IOException {
+            int c;
+            while ((c = read()) <= ' ')
+                if (c == -1)
+                    return null;
+
+            StringBuilder sb = new StringBuilder();
+            sb.append((char) c);
+
+            while ((c = read()) > ' ')
+                sb.append((char) c);
+
+            return sb.toString();
+        }
+    }
+    public static void main(String[] args) throws IOException{
+        FastScanner fs = new FastScanner();
+        PrintWriter out = new PrintWriter(System.out);
+        int n = fs.nextInt();
+        ArrayList<Integer> arr = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            int x = fs.nextInt();
+            arr.add(x);
+        }
+        Collections.sort(arr);
+        Long sum = (long) 0;
+        for (Integer a : arr) {
+            if (a > sum + 1) break;
+            sum += a;
+        }
+        out.print(sum + 1);
+        out.flush();
+    }
+}
