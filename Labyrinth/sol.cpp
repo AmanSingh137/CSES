bdc99f8edeed17a3caa7f8ca92a6416db9e4a6fb
Aman Kumar Singh <singhaman13720@gmail.com>
2025-12-22 22:36:00 +0530

    TLE in just 1 case

--- Labyrinth/sol.cpp
+++ Labyrinth/sol.cpp
@@ -9,7 +9,6 @@ int main() {
     int n, m;
     cin >> n >> m;
     vector<string> vec;
-    vector<string> results;
     for (int i = 0; i < n; i++) {
         string t;
         cin >> t;
@@ -21,6 +20,7 @@ int main() {
         int fl=0;
         for (int j = 0; j < m; j++) {
             if (vec[i][j]=='A') {
+                vis[i][j]=1;
                 st.push({{i,j}, ""});
                 fl=1;
                 break;
@@ -28,6 +28,8 @@ int main() {
         }
         if (fl) break;
     }
+    string results = "";
+    vector<vector<pair<int, int>>> parent;
     while(!st.empty()) {
         auto p = st.front();
         st.pop();
@@ -35,27 +37,28 @@ int main() {
         int y = p.first.second;
         string cur = p.second;
         if (vec[x][y]=='B') {
-            results.push_back(cur);
+            results = cur;
             break;
         }
-        if (vis[x][y]==1) continue;
-        vis[x][y]=1;
         int a[] = {0, 0, -1, 1};
         int b[] = {-1, 1, 0, 0};
         char c[] = {'L', 'R', 'U', 'D'};
         for (int i = 0; i < 4; i++) {
             if (x+a[i] < 0 || y+b[i] < 0 || x+a[i]>=n || y+b[i]>=m) continue;
+            if (vis[x+a[i]][y+b[i]]==1 || vec[x+a[i]][y+b[i]]=='#') continue;
             else if (vec[x+a[i]][y+b[i]]=='.' || vec[x+a[i]][y+b[i]]=='B') {
+                vis[x+a[i]][y+b[i]]=1;
                 st.push({{x+a[i], y+b[i]}, cur+c[i]});
             }
         }
     }
-    if(results.size()==0) {
+    
+    if (results.size()==0) {
         cout << "NO" << endl;
         return 0;
     }
-    
-    cout << "YES" << endl << results[0].size() << endl << results[0] << endl;
+
+    cout << "YES" << endl << results.size() << endl << results << endl;
     return 0;
 }
 
