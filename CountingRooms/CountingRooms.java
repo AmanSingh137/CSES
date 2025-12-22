3e875c4dd4ebb5bee67bbd45c86fb2a8c5db671c
Aman Kumar Singh <singhaman13720@gmail.com>
2025-12-22 08:51:25 +0530

    solving counting rooms in java

---
+++ CountingRooms/CountingRooms.java
@@ -0,0 +1,108 @@
+package CountingRooms;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
+public class CountingRooms {
+    static class FastScanner {
+        private final InputStream in = System.in;
+        private final byte[] buffer = new byte[1 << 16];
+        private int ptr = 0, len = 0;
+
+        private int read() throws IOException {
+            if (ptr >= len) {
+                len = in.read(buffer);
+                ptr = 0;
+                if (len <= 0)
+                    return -1;
+            }
+            return buffer[ptr++];
+        }
+
+        int nextInt() throws IOException {
+            int c;
+            while ((c = read()) <= ' ')
+                if (c == -1)
+                    return -1;
+            int sign = 1;
+            if (c == '-') {
+                sign = -1;
+                c = read();
+            }
+            int val = c - '0';
+            while ((c = read()) > ' ')
+                val = val * 10 + (c - '0');
+            return val * sign;
+        }
+
+        long nextLong() throws IOException {
+            int c;
+            while ((c = read()) <= ' ')
+                if (c == -1)
+                    return -1;
+            int sign = 1;
+            if (c == '-') {
+                sign = -1;
+                c = read();
+            }
+            long val = c - '0';
+            while ((c = read()) > ' ')
+                val = val * 10 + (c - '0');
+            return val * sign;
+        }
+        String nextString() throws IOException {
+            int c;
+            while ((c = read()) <= ' ')
+                if (c == -1)
+                    return null;
+
+            StringBuilder sb = new StringBuilder();
+            sb.append((char) c);
+
+            while ((c = read()) > ' ')
+                sb.append((char) c);
+
+            return sb.toString();
+        }
+    }
+    public static void solve (int[][] vis, char[][] a, int i, int j, int n, int m) {
+        if (i < 0 || j < 0 || i >= n || j >= m) return;
+        if (a[i][j]=='#') return;
+        if (vis[i][j]==1) return;
+        vis[i][j]=1;
+        solve(vis, a, i, j+1, n, m);
+        solve(vis, a, i, j-1, n, m);
+        solve(vis, a, i-1, j, n, m);
+        solve(vis, a, i+1, j, n, m);
+    }
+    public static void main(String[] args) throws IOException {
+        FastScanner fs = new FastScanner();
+        PrintWriter out = new PrintWriter(System.out);
+
+        int n = fs.nextInt(), m = fs.nextInt();
+        char[][] a = new char[n][m];
+        ArrayList<String> arr = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            arr.add(fs.nextString());
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                a[i][j] = arr.get(i).charAt(j);
+            }
+        }
+        int cnt = 0;
+        int[][] vis = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (vis[i][j]==0 && a[i][j] == '.') {
+                    solve(vis, a, i, j, n, m);
+                    cnt++;
+                }
+            }
+        }
+        out.print(cnt);
+        out.flush();
+    }
+}
